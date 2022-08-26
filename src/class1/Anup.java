2aba829b5d3abe3c640d4980200349075bc33ff0
Anup Kakde <prajyotkakde2410@gmail.com>
2022-08-26 19:31:15 +0530

    Hii, Varsha(T.L)
    I consider your suggestion, i added one more loop for another name. so
    please Review and Approve it.
    
    Regards
    Prajyot Kakde.

--- src/class1/Anup.java
+++ src/class1/Anup.java
@@ -5,14 +5,21 @@ public class Anup {
 	public static void main(String[] args) {
 		
 	String name="varsha";
+	String nm="anup";
 	int l=name.length();
+	int a=nm.length();
 	
 	for(int i=l-1;i>=0;i--) 
 	{
 		char c=name.charAt(i);
 		System.out.print(c);
-	}
 		
+	}
+		for(int i=a-1;i>=0;i--)
+		{
+			char d=nm.charAt(i);
+			System.out.print(d);
+		}
 
 	}
 
