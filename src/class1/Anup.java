6c81335224997ad8eff97884bbfcb84ad86d1119
Anup Kakde <prajyotkakde2410@gmail.com>
2022-08-26 16:10:46 +0530

    Commited to new branch i.e Anup_Branch

---
+++ src/class1/Anup.java
@@ -0,0 +1,19 @@
+package class1;
+
+public class Anup {
+
+	public static void main(String[] args) {
+		
+	String name="varsha";
+	int l=name.length();
+	
+	for(int i=l-1;i>=0;i--) 
+	{
+		char c=name.charAt(i);
+		System.out.print(c);
+	}
+		
+
+	}
+
+}
