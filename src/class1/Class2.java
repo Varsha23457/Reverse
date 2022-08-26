a717e7e13c56f019598dcbc9260eedc4ecbca6a2
Varsha23457 <110117730+Varsha23457@users.noreply.github.com>
2022-08-26 13:45:23 +0530

    Hello please review it

---
+++ src/class1/Class2.java
@@ -0,0 +1,19 @@
+package class1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		
+	String name="varsha";
+	int l=name.length();
+	
+	for(int i=l-1;i>=0;i--) 
+	{
+		char c=name.charAt(i);
+		System.out.print(c);
+	}
+		
+
+	}
+
+}
